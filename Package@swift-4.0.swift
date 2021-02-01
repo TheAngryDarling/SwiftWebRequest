c03b6f330044d8ecc89eeb7e0d973d62f65fc5f4
Tyler Anger <30189019+TheAngryDarling@users.noreply.github.com>
2021-02-01 14:05:48 -0500

    Added dependency for Swifter.  Due to Swifter Swift version support, Package.swift had to be broken out into separate files for different Swift versions to get Swifter to compile properly

--- Package@swift-4.0.swift
+++ Package@swift-4.0.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:4.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["WebRequest"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/httpswift/swifter.git", from: "1.5.0")
+        .package(url: "https://github.com/httpswift/swifter.git", .exact("1.4.5"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
