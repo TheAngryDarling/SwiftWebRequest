358dc03f3830d687cbba531878cde6aa1727db1d
Tyler Anger <30189019+TheAngryDarling@users.noreply.github.com>
2021-06-27 19:05:17 -0400

    Added missing Swift Version specific Package.swift files.

---
+++ Package@swift-4.1.swift
@@ -0,0 +1,27 @@
+// swift-tools-version:4.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "WebRequest",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "WebRequest",
+            targets: ["WebRequest"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/httpswift/swifter.git", .exact("1.4.5"))
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "WebRequest",
+            dependencies: []),
+        .testTarget(
+            name: "WebRequestTests",
+            dependencies: ["WebRequest", "Swifter"]),
+    ]
+)
