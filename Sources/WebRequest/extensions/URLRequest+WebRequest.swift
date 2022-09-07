e72b462b5ef9d4bfc258f049bc4d49d57ea74819
Tyler Anger <30189019+TheAngryDarling@users.noreply.github.com>
2022-09-07 12:31:43 -0400

    Simplified internal custom URLRequest.init(url: URL,_ oldRequest: URLRequest) to just completely copy the oldRequest (assign self it it) and then update the self.url property afterwards.  This reduced the code to 2 lines as well as removes any #if _runtime and if #available statements

--- Sources/WebRequest/extensions/URLRequest+WebRequest.swift
+++ Sources/WebRequest/extensions/URLRequest+WebRequest.swift
@@ -16,24 +16,7 @@ internal extension URLRequest {
     /// Create new URLRequest copying old request details
     init(url: URL,
          _ oldRequest: URLRequest) {
-        self.init(url: url,
-                  cachePolicy: oldRequest.cachePolicy,
-                  timeoutInterval: oldRequest.timeoutInterval)
-        
-        self.allHTTPHeaderFields = oldRequest.allHTTPHeaderFields
-        self.allowsCellularAccess = oldRequest.allowsCellularAccess
-        #if _runtime(_ObjC)
-            if #available(OSX 10.15, *) {
-                self.allowsConstrainedNetworkAccess = oldRequest.allowsConstrainedNetworkAccess
-            }
-            if #available(OSX 10.15, *) {
-                self.allowsExpensiveNetworkAccess = oldRequest.allowsExpensiveNetworkAccess
-            }
-        #endif
-        self.httpBody = oldRequest.httpBody
-        self.httpMethod = oldRequest.httpMethod
-        self.httpShouldHandleCookies = oldRequest.httpShouldHandleCookies
-        self.httpShouldUsePipelining = oldRequest.httpShouldUsePipelining
-        self.networkServiceType = oldRequest.networkServiceType
+        self = oldRequest
+        self.url = url
     }
 }
